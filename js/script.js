ec976301bb43eea7f8efe6914dc406ee1b09dfe0
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2025-09-25 16:46:15 +0200

    fix: added new functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function isPalindrome(parola) {
-    for(let i = 0; i < Math.floor(parola.length/2); i++) {
+    for (let i = 0; i < Math.floor(parola.length / 2); i++) {
         if (parola[i] != parola[parola.length-1-i]) {
             return false;
         }
@@ -7,38 +7,25 @@ function isPalindrome(parola) {
     return true;
 }
 
-function pariODispari() {
-    let scelta = prompt("Pari o dispari: \nDigita 'p' per pari o 'd' per dispari:\n");
-    while (scelta !== "p" && scelta !== "d") {
-        print("valore non valido");
-        scelta = prompt("Pari o dispari: \nDigita 'p' per pari o 'd' per dispari:\n");
-    }
-    let numeroScelto = prompt("Inserisci un numero da 1 a 6:\n");
-    while (numeroScelto < 1 || numeroScelto > 6) {
-        print("valore non valido");
-        numeroScelto = prompt("Inserisci un numero da 1 a 6:\n");
-    }
-    const myNumber = Math.floor(Math.random() * 6) + 1
 
-    if (scelta === "p" && (myNumber + numeroScelto) % 2 === 0) {
-        console.log("Hai vinto");
-    } else if (scelta === "d" && (myNumber + numeroScelto) % 2 != 0) {
-        console.log("Hai vinto");
-    } else {
-        console.log("Hai perso");
-    }
+function numberGenerator() {
+    const numero = Math.floor(Math.random() * 6) + 1;
+    return numero;
 }
 
 
+function isEven(numero) {
+    return numero % 2 === 0;
+}
 
-// Invocazioni delle funzioni
 
+// Esercizio Palindromi
 let parola = prompt("Inserisci una parola:\n");
 
 while (parola.length === 0) {
-        print("parola non valida");
-        parola = prompt("Inserisci una parola:\n");
-    }
+    print("parola non valida");
+    parola = prompt("Inserisci una parola:\n");
+}
 
 if (isPalindrome(parola)) {
     console.log("La parola è palindroma");
@@ -46,7 +33,28 @@ if (isPalindrome(parola)) {
     console.log("La parola non è palindroma");
 }
 
-pariODispari();
+
+// Esercizio Pari o dispari
+let scelta = prompt("Pari o dispari: \nDigita 'p' per pari o 'd' per dispari:\n");
+while (scelta !== "p" && scelta !== "d") {
+    print("valore non valido");
+    scelta = prompt("Pari o dispari: \nDigita 'p' per pari o 'd' per dispari:\n");
+}
+
+let numeroScelto = prompt("Inserisci un numero da 1 a 6:\n");
+while (numeroScelto < 1 || numeroScelto > 6) {
+    print("valore non valido");
+    numeroScelto = prompt("Inserisci un numero da 1 a 6:\n");
+}
+
+const numeroPc = numberGenerator();
+const somma = numeroPc + numeroScelto;
+if ((isEven(somma) && scelta === "p") || (!isEven(somma) && scelta === "d")) {
+    console.log("Hai vinto");
+} else {
+    console.log("Hai perso");
+    
+}
 
 
 
